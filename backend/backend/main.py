ec9c514c5e5ede1fe89f9b7ee0ad4ff46f0bc0f4
Heber Junior <heberjr2001@gmail.com>
2021-11-25 23:17:39 -0300

    atualização

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -4,8 +4,8 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+from backend import crud, models, schemas
+from backend.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
     
@@ -32,3 +32,50 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
+
+#informação do usuário
+@app.get("/users/{user_id}", response_model=schemas.User)
+def find_user(user_id: int, db: Session = Depends(get_db)):
+    return crud.get_user(db, user_id)
+
+#editar usuário
+@app.put("/users/{user_id}", response_model=schemas.User)
+async def edit_user(user_id: int, new_value: schemas.UserEdit, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user:
+        return crud.edit_user(db, user_id, new_value=new_value)
+    
+    raise HTTPException(status_code=400, detail="Usuário não existe")
+
+
+@app.patch("/users/{user_id}", response_model=schemas.User)
+async def edit_user(user_id: int, new_value: schemas.UserEdit, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user:
+        return crud.edit_user(db, user_id, new_value=new_value)
+    
+    raise HTTPException(status_code=400, detail="Usuário não existe")
+
+#medidas
+@app.get("/measures/", response_model=List[schemas.Measure])
+def read_measures(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_measures(db, skip=skip, limit=limit)
+    return users
+
+
+@app.post("/users/{user_id}/measures/", response_model=schemas.Measure)
+def create_measure(user_id: int, measure: schemas.MeasureCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user:
+        return crud.create_user_measure(db, measure, user_id)
+
+    raise HTTPException(status_code=400, detail="Usuário inexistente")
+
+
+@app.get("/users/{user_id}/measures/", response_model=List[schemas.Measure])
+def get_user_measure(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user:
+        return crud.get_user_measure(db, user_id)
+
+    raise HTTPException(status_code=400, detail="Usuário não existe")
